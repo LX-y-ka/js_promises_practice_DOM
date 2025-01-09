79a851d4dbafb85c1c0eac7378b46b68eca9028e
LX-y-ka <alexandravirtualife@gmail.com>
2025-01-09 12:18:08 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,78 @@
 'use strict';
+
+const firstPromise = new Promise((resolve, reject) => {
+  document.addEventListener('click', () => {
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    reject(new Error('First promise was rejected'));
+  }, 3000);
+});
+
+const secondPromise = new Promise((resolve, reject) => {
+  document.addEventListener('mousedown', (e) => {
+    if (e.button === 0 || e.button === 2) {
+      resolve('Second promise was resolved');
+    }
+  });
+});
+
+const thirtPromise = new Promise((resolve, reject) => {
+  document.addEventListener('mousedown', (e) => {
+    if (e.button === 3) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+firstPromise.then(
+  (message) => {
+    const div = document.createElement('div');
+
+    div.setAttribute('data-qa', 'notification');
+    div.innerText = message;
+    document.body.appendChild(div);
+  },
+  (error) => {
+    const div = document.createElement('div');
+
+    div.setAttribute('data-qa', 'notification');
+    div.innerText = error.message;
+    document.body.appendChild(div);
+  },
+);
+
+secondPromise.then(
+  (message) => {
+    const div = document.createElement('div');
+
+    div.setAttribute('data-qa', 'notification');
+    div.innerText = message;
+    document.body.appendChild(div);
+  },
+  (error) => {
+    const div = document.createElement('div');
+
+    div.setAttribute('data-qa', 'notification');
+    div.innerText = error.message;
+    document.body.appendChild(div);
+  },
+);
+
+thirtPromise.then(
+  (message) => {
+    const div = document.createElement('div');
+
+    div.setAttribute('data-qa', 'notification');
+    div.innerText = message;
+    document.body.appendChild(div);
+  },
+  (error) => {
+    const div = document.createElement('div');
+
+    div.setAttribute('data-qa', 'notification');
+    div.innerText = error.message;
+    document.body.appendChild(div);
+  },
+);
